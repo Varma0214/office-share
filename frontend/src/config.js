39d196daebdfe092428ae0dd04b8d0bcfbf5228b
ravivarma <varmagollapalli@gmail.com>
2026-05-17 19:56:56 +0530

    url updated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // frontend/src/config.js
 
 // Replace this with your actual deployed Vercel backend URL
-export const API_BASE_URL = "https://office-share-kappa.vercel.app/api";
+export const API_BASE_URL = "https://office-share-1.onrender.com/api";
